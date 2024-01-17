a56678b9ca888c36367c222edc548703ac1a1b52
Guy Royse <guy@guyroyse.com>
2024-01-17 15:52:48 -0500

    refactored and added a count option

--- api/app.js
+++ api/app.js
@@ -1,35 +1,27 @@
-// Import express module
 import express from 'express'
-import { embedAndSave, queryRedis } from './embed.js'
+import { search, save } from './embed.js'
 
-// Create an instance of express
+const EXPRESS_PORT = 3000
+
 const app = express()
 
-// Use express.json() middleware for parsing JSON bodies
+// use middleware for automagic parsing of JSON bodies
 app.use(express.json())
-
-// Define a GET route
-app.get('/', (req, res) => {
-  res.send('Hello, World!')
-})
 
 app.post('/search', async (req, res) => {
   const query = req.body.query
-  const results = await queryRedis(query)
+  const count = req.body.count ?? 1
+  const results = await search(query, count)
   res.send(results)
 })
 
-// Define a POST route
 app.post('/load', async (req, res) => {
   const sighting = req.body
-
-  console.log('Received sighting:', sighting.observed)
-  const summary = await embedAndSave(sighting)
-  res.send(summary)
+  const result = await save(sighting)
+  res.send({ "response": result })
 })
 
-// Start the server on port 3000
-const PORT = 3000
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+
+app.listen(EXPRESS_PORT, () => {
+  console.log(`Server running on port ${EXPRESS_PORT}`)
 })
--- api/embed.js
+++ api/embed.js
@@ -22,39 +22,48 @@ const vectorStore = new RedisVectorStore(embeddingModel, {
   keyPrefix: "bigfoot:sighting:",
 })
 
-export async function embedAndSave(sighting) {
+export async function save(sighting) {
 
-  // const instructionTemplate = `
-  //   You are a helpful assistant who summarize accounts of Bigfoot sightings. These
-  //   summaries will be used to generate embeddings for the sightings so that they can be
-  //   searched using vector search. You will be given an account of a Bigfoot sighting and
-  //   you will summarize it using no more than 512 words. You will ONLY return the summary,
-  //   and nothing more. You will NEVER return more than 512 words.`
-
-  // const sightingTemplate = "{sighting}"
-
-  // const chatPrompt = ChatPromptTemplate.fromMessages([
-  //   ["system", instructionTemplate],
-  //   ["system", sightingTemplate]
-  // ])
-
-  // const summarizationModel = new LlamaCpp({
-  //   modelPath: "./models/mistral-7b-instruct-v0.2.Q4_K_M.gguf"
-  // })
-
-  // const summary = await chatPrompt.pipe(summarizationModel).invoke({
-  //   sighting: sighting.observed
-  // })
+  // const summary = await summarize(sighting.observed)
+  const summary = sighting.observed
 
   await vectorStore.addDocuments([{
     metadata: sighting,
-    pageContent: sighting.observed
+    pageContent: summary
   }])
 
   return `Added sighting ${sighting.id} added to vector store`
 }
 
-export async function queryRedis(text) {
-  const result = await vectorStore.similaritySearch(text, 1)
+export async function search(query, count) {
+  const result = await vectorStore.similaritySearch(query, count)
   return result
+}
+
+async function summarize(text) {
+
+  // TODO: instantiate model outside of this function
+  const summarizationModel = new LlamaCpp({
+    modelPath: "./models/mistral-7b-instruct-v0.2.Q4_K_M.gguf"
+  })
+
+  const instructionTemplate = `
+    You are a helpful assistant who summarize accounts of Bigfoot sightings. These
+    summaries will be used to generate embeddings for the sightings so that they can be
+    searched using vector search. You will be given an account of a Bigfoot sighting and
+    you will summarize it using no more than 512 words. You will ONLY return the summary,
+    and nothing more. You will NEVER return more than 512 words.`
+
+  const sightingTemplate = "{sighting}"
+
+  const chatPrompt = ChatPromptTemplate.fromMessages([
+    ["system", instructionTemplate],
+    ["system", sightingTemplate]
+  ])
+
+  const summary = await chatPrompt.pipe(summarizationModel).invoke({
+    sighting: text
+  })
+
+  return summary
 }
