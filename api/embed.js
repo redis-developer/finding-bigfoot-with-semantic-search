0c834c1dc3c17cfcd859655168bad9e7b2190de2
Guy Royse <guy@guyroyse.com>
2024-01-24 15:49:59 -0500

    removed summarization and creation of embedding

--- api/embed.js
+++ api/embed.js
@@ -1,19 +1,8 @@
-import { ChatPromptTemplate } from '@langchain/core/prompts'
 
-import { fetchEmbeddingModel, fetchSummarizationModel } from './models.js'
+import { fetchEmbeddingModel } from './models.js'
 import { redis } from './redis.js'
 
 
-const instructionTemplate = `
-  You are a helpful assistant who summarize accounts of Bigfoot sightings. These
-  summaries will be used to generate embeddings for the sightings so that they can be
-  searched using vector search. You will be given an account of a Bigfoot sighting and
-  you will summarize it using no more than 512 words. You will ONLY return the summary,
-  and nothing more. You will NEVER return more than 512 words.`
-
-const sightingTemplate = "{sighting}"
-
-// TODO: have this dump to a stream instead and process it asynchronously
 export async function save(sighting) {
 
   // set the basic information in Redis
@@ -23,16 +12,12 @@ export async function save(sighting) {
   // if there is observed text...
   if (sighting.observed) {
 
-    // ...summarize and embed it
-    const summary = await summarize(sighting.observed)
-    const embeddingBytes = await embed(summary)
+    // ...dump the text into the summarization queue
+    const { id, observed } = sighting
+    await redis.xAdd('bigfoot:sighting:reported', '*', { id, observed })
 
-    // ...save it to Redis
-    redis.hSet(key, 'summary', summary)
-    await redis.hSet(key, 'embedding', embeddingBytes)
-
-    // ...and return the results
-    return `Added sighting ${sighting.id} to vector store using summary:\n${summary}`
+    // ...and log that you did
+    return `Added sighting ${sighting.id} to backlog for summarization.`
   }
 
   return `Added sighting ${sighting.id} to store without summary.`
@@ -65,28 +50,4 @@ export async function search(query, params, count) {
   const results =  searchResults.documents.map(document => document.value)
 
   return results
-}
-
-async function summarize(text) {
-  try {
-    return await tryToSummarize(fetchSummarizationModel(), text)
-  } catch (error) {
-    console.log("Error using model. Recreating model and retrying.", error)
-    return await innerSummarize(fetchSummarizationModel(true), text)
-  }
-}
-
-async function tryToSummarize(model, text) {
-  return await ChatPromptTemplate
-    .fromMessages([
-      ["system", instructionTemplate],
-      ["system", sightingTemplate] ])
-    .pipe(model)
-    .invoke({ sighting: text })
-}
-
-async function embed(text) {
-  const embedding = await fetchEmbeddingModel().embedQuery(text)
-  const embeddingBytes = Buffer.from(Float32Array.from(embedding).buffer)
-  return embeddingBytes
 }
--- api/models.js
+++ api/models.js
@@ -1,18 +1,7 @@
 import { HuggingFaceTransformersEmbeddings } from '@langchain/community/embeddings/hf_transformers'
-import { LlamaCpp } from '@langchain/community/llms/llama_cpp'
 
-let summarizationModel = null
+
 let embeddingModel = null
-
-export function fetchSummarizationModel(cacheBust = false) {
-  if (summarizationModel && !cacheBust) return summarizationModel
-  summarizationModel = new LlamaCpp({
-    modelPath: 'models/mistral-7b-instruct-v0.2.Q4_K_M.gguf',
-    batchSize: 8192,
-    contextSize: 8192
-  })
-  return summarizationModel
-}
 
 export function fetchEmbeddingModel(cacheBust = false) {
   if (embeddingModel && !cacheBust) return embeddingModel
