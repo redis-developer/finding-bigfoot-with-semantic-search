64c31860683b9885088ea113709e1bb80f49c4de
Guy Royse <guy@guyroyse.com>
2024-01-25 14:45:50 -0500

    a more robust start of a dockerized app

--- api/redis.js
+++ api/redis.js
@@ -2,13 +2,43 @@ import { SchemaFieldTypes, VectorAlgorithms, createClient } from 'redis'
 
 import { REDIS_HOST, REDIS_PORT } from './config.js'
 
+
 // connect to redis
 export const redis = createClient({ socket: { host: REDIS_HOST, port: REDIS_PORT } })
 redis.on('error', (err) => console.log('Redis Client Error', err))
 await redis.connect()
 
+// wait for Redis to finish loading
+await waitForRedis()
+
 // create index if needed
 if (!await indexExists()) await createIndex()
+
+
+/**
+ * Wait for Redis to finish loading.
+ */
+async function waitForRedis() {
+  let loaded = false
+
+  while (!loaded) {
+    loaded = await isReady()
+    if (!loaded) console.log("Redis is loading...")
+  }
+
+  console.log("Redis is ready!")
+}
+
+/**
+ * Determine if Redis is done loading or not.
+ */
+export async function isReady() {
+  const info = await redis.info('persistence')
+  const lines = info.split('\r\n')
+  const loadingLine = lines.find(line => line.startsWith('loading:'))
+  const loadingValue = loadingLine.split(':')[1]
+  return loadingValue === '0'
+}
 
 /**
  * Determines if the index has been created or not.
