b6a7b4ff0fbaeff51fab86dcaee28e6c0989fd8c
Simon Green <simon@captaincodeman.com>
2024-02-12 13:17:33 -0700

    add tailwindcss

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,13 +1,15 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-node'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: vitePreprocess(),
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
-};
+		adapter: adapter(),
+	},
+}
 
-export default config;
+export default config
