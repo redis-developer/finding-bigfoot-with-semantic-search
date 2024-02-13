265cbbef8af5e0e08261f7733250a16bbe781be3
Simon Green <simon@captaincodeman.com>
2024-02-13 10:22:59 -0700

    add page title

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -2,6 +2,10 @@
   import "../app.pcss"
 </script>
 
+<svelte:head>
+  <title>Bigfoot Sightings!</title>
+</svelte:head>
+
 <div class="m-4 container mx-auto px-4">
   <nav>
     <h1 class="text-slate-700 text-4xl font-extrabold tracking-tight">Bigfoot Sightings!</h1>
