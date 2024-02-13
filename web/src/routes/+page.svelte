c19e65a86e5e68a61fa9e3e36aa6f02b25c916ba
Simon Green <simon@captaincodeman.com>
2024-02-13 10:17:36 -0700

    default to 12 results

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -83,7 +83,7 @@
   <div class="sm:col-span-2">
     <label for="count" class="block text-sm font-medium leading-6 text-gray-900">Count</label>
     <div class="mt-1">
-      <input type="number" name="count" id="count" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+      <input type="number" name="count" id="count" value="12" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
     </div>
   </div>
 
