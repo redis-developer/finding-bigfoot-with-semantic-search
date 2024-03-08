39943bb9d1ae43e6c7869332350b1db5c0ffe452
Guy Royse <guy@guyroyse.com>
2024-03-08 17:26:24 -0500

    removed embedded for docker for performance reasons

--- web/src/config.js
+++ web/src/config.js
@@ -1,5 +1,2 @@
-console.log(import.meta.env)
-
-
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ?? '/api'
 export const RESULT_COUNT = Number(import.meta.env.VITE_RESULT_COUNT ?? 10)
